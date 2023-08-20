64c0147f9495fe89d0c55ec9502b436effff417f
Khushi-1703 <102543578+Khushi-1703@users.noreply.github.com>
2023-08-20 16:11:31 +0530

    Update MintToken.sol

--- MintToken.sol
+++ MintToken.sol
@@ -5,49 +5,32 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CustomToken is ERC20, Ownable {
-    // Constructor to initialize the token with a name and symbol
+    event TokensMinted(address indexed to, uint256 amount);
+    event TokensBurned(address indexed from, uint256 amount);
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        // Mint initial supply of tokens to the contract deployer (owner)
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
-    // Function to allow the contract owner to mint new tokens to a provided address
     function mint(address to, uint256 amount) public onlyOwner {
         require(to != address(0), "Invalid address");
         _mint(to, amount);
+        emit TokensMinted(to, amount);
     }
 
-    // Function to allow any user to burn (destroy) their own tokens
     function burn(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        
         _burn(msg.sender, amount);
+        emit TokensBurned(msg.sender, amount);
     }
 
-    // Override the transfer function to add additional checks
-    function transfer(address to, uint256 amount) public override returns (bool) {
+    function transferTokens(address to, uint256 amount) public {
         require(to != address(0), "Invalid address");
         require(amount > 0, "Amount must be greater than zero");
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
         
         _transfer(msg.sender, to, amount);
-        return true;
-    }
-
-    // Function to transfer tokens on behalf of the sender (requires approval)
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        require(from != address(0), "Invalid sender address");
-        require(to != address(0), "Invalid recipient address");
-        require(amount > 0, "Amount must be greater than zero");
-        require(balanceOf(from) >= amount, "Insufficient balance");
-        require(allowance(from, msg.sender) >= amount, "Allowance exceeded");
-        
-        _transfer(from, to, amount);
-        _approve(from, msg.sender, allowance(from, msg.sender) - amount);
-        return true;
-    }
-    
-    // Function to approve a spender to spend a certain amount of tokens on behalf of the sender
-    function approve(address spender, uint256 amount) public override returns (bool) {
-        _approve(msg.sender, spender, amount);
-        return true;
     }
 }
